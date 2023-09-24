df5be5380c542e4ca42c3a27f8919f628c0844d8
Vitaliy Shatalov <vvshatalov@avito.ru>
2023-09-24 09:50:36 +0700

    sez

--- apps/bot/nlpMiddlewares.ts
+++ apps/bot/nlpMiddlewares.ts
@@ -4,6 +4,7 @@ import { presentScriptToMiddleware } from '@libs/shared/scripts'
 import { handleNLPScript } from '@libs/scripts/handleNLP'
 import { scenarioActions } from '@libs/nlp/scenarios/list'
 import axios from 'axios'
+import { GrammyDialog } from '@libs/shared/dialog'
 
 const PYTHON_SERVICE_URL =
   process.env.BOT_PYTHON_ML_URL ?? 'http://localhost:5001'
@@ -51,6 +52,9 @@ export const initNLPMiddlewares = async () => {
         })
         .then(r => r.data?.answer)
         .catch(e => undefined)
+
+      console.log('ml answer', answer)
+      let needToSolve = true
       if (!answer) {
         await ctx.reply(
           'К сожалению, я не могу подобрать наилучший ответ для Ваc в данный момент\n\n' +
@@ -58,16 +62,40 @@ export const initNLPMiddlewares = async () => {
             ? 'Ознакомьтесь с наименее релевантными ответами:'
             : 'Обратитесь, пожалуйста, позднее',
         )
+      } else {
+        const dialog = new GrammyDialog(ctx)
+        const confirmation = await dialog
+          .prompt(`Этот ответ вам должен подойти:\n\n<i>${answer}</i>`, {
+            keyboard: [
+              {
+                yes: 'Спасибо, ответ помог',
+                no: 'Ответ не помог',
+              },
+            ],
+          })
+          .then(d => d.message)
+
+        if (confirmation === 'keyboard:yes') {
+          await dialog.answer('Рад, что получилось помочь')
+          needToSolve = false
+        } else if (confirmation === 'keyboard:no') {
+          needToSolve = true
+        } else if (!confirmation.startsWith('keyboard')) {
+          needToSolve = false
+        }
+      }
+      if (needToSolve) {
+        console.log('need to solve')
         if (badClasses.length) {
           await presentScriptToMiddleware(handleNLPScript)(ctx, next, {
             result,
-            badClasses,
+            classes: badClasses,
             nlp,
           })
+        } else {
+          await ctx.reply('Сожалею об этом...')
         }
-        return
       }
-      await ctx.reply(`Этот ответ вам должен подойти:\n\n${answer}`)
       return
     } else if (utilityMessage) {
       await ctx.reply(result.answer)
